ab35bc68f1590c5b90c03812b22ef3aeeb5ba8e2
Mehmet Akgul <mehmetakgul.dev@gmail.com>
2023-01-12 21:26:49 +0300

    AdminController.java    @GetMapping("/all")    endpointi eklendi

--- src/main/java/com/project/schoolmanagment/controller/AdminController.java
+++ src/main/java/com/project/schoolmanagment/controller/AdminController.java
@@ -3,11 +3,14 @@ package com.project.schoolmanagment.controller;
 import com.project.schoolmanagment.entity.concretes.Admin;
 import com.project.schoolmanagment.service.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("admin")
@@ -18,13 +21,21 @@ public class AdminController {
 
     @PostMapping("/save")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
-    public ResponseEntity<?> save(@RequestBody @Valid Admin admin){
+    public ResponseEntity<?> save(@RequestBody @Valid Admin admin) {
         return ResponseEntity.ok(adminService.save(admin));
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public ResponseEntity<List<Admin>> getAll() {
+        List<Admin> author = adminService.getAllAdmin();
+        return new ResponseEntity<>(author, HttpStatus.OK);
+
     }
 
     @DeleteMapping("/delete/{id}")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
-    public ResponseEntity<String> delete(@PathVariable Long id){
+    public ResponseEntity<String> delete(@PathVariable Long id) {
         return ResponseEntity.ok(adminService.deleteAdmin(id));
     }
 
