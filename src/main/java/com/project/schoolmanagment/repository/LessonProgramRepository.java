c4ba260844f985f3d157730f86140863f4604f5c
Mehmet Akgul <mehmetakgul.dev@gmail.com>
2023-01-12 22:44:43 +0300

    id ile yapilan sorgular ssn ile yapilacak sekilde guncellendi

--- src/main/java/com/project/schoolmanagment/repository/LessonProgramRepository.java
+++ src/main/java/com/project/schoolmanagment/repository/LessonProgramRepository.java
@@ -1,23 +1,23 @@
 package com.project.schoolmanagment.repository;
 
 import com.project.schoolmanagment.entity.concretes.LessonProgram;
-import com.project.schoolmanagment.entity.concretes.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
-public interface LessonProgramRepository extends JpaRepository<LessonProgram,Long> {
+public interface LessonProgramRepository extends JpaRepository<LessonProgram, Long> {
 
     @Query(value = "SELECT l FROM LessonProgram l WHERE l.id IN :lessonProgramIdList")
     Set<LessonProgram> getLessonProgramByLessonProgramIdList(@Param("lessonProgramIdList")
                                                              Set<Long> lessonProgramIdList);
 
-    List<LessonProgram> getLessonProgramByTeachersId(Long teacherId);
-    List<LessonProgram> getLessonProgramByStudentsId(Long studentId);
+    @Query("select l from LessonProgram l inner join l.students students where students.ssn = ?1")
+    List<LessonProgram> getLessonProgramByStudentSsn(String ssn);
 
 
+    @Query("select l from LessonProgram l inner join l.teachers teachers where teachers.ssn = ?1")
+    List<LessonProgram> getLessonProgramByTeacherSsn(String ssn);
 }
