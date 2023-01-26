b92d19bc8c472859eb1b81e0bc0176e9d645065f
Mehmet Akgul <mehmetakgul.dev@gmail.com>
2023-01-27 00:03:00 +0300

    unnecessary imports cleared

--- src/main/java/com/project/schoolmanagment/payload/Dto/DeanDto.java
+++ src/main/java/com/project/schoolmanagment/payload/Dto/DeanDto.java
@@ -1,21 +1,19 @@
 package com.project.schoolmanagment.payload.Dto;
 
 import com.project.schoolmanagment.entity.concretes.Dean;
-import com.project.schoolmanagment.entity.concretes.ViceDean;
-import com.project.schoolmanagment.payload.request.DeanRequest;
-import com.project.schoolmanagment.payload.request.ViceDeanRequest;
-import lombok.Data;
+ import com.project.schoolmanagment.payload.request.DeanRequest;
+ import lombok.Data;
 
 @Data
 public class DeanDto {
 
     public Dean dtoDean(DeanRequest deanRequest) {
         return Dean.builder()
-                .birthDay(deanRequest.getBirthDay())
                 .name(deanRequest.getName())
                 .surname(deanRequest.getSurname())
                 .password(deanRequest.getPassword())
                 .ssn(deanRequest.getSsn())
+                .birthDay(deanRequest.getBirthDay())
                 .birthPlace(deanRequest.getBirthPlace())
                 .phoneNumber(deanRequest.getPhoneNumber())
                 .gender(deanRequest.getGender())
