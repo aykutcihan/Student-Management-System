4f2ef0f5b53e66a2e1eeb0dcf95d90d9d2201974
Mehmet Akgul <mehmetakgul.dev@gmail.com>
2023-02-24 22:40:50 +0300

    Messages.java updated

--- src/main/java/com/project/schoolmanagment/utils/Messages.java
+++ src/main/java/com/project/schoolmanagment/utils/Messages.java
@@ -12,20 +12,25 @@ public class Messages {
     public static final String ALREADY_REGISTER_MESSAGE_USERNAME = "Error: User with username %s already register";
     public static final String ALREADY_REGISTER_MESSAGE_STUDENT_NUMBER = "Error: Student with student number %s already register";
     public static final String LESSON_PROGRAM_NOT_FOUND_MESSAGE = "Error: Lesson Programs not found";
-    public static final String  ALREADY_REGISTER_MESSAGE_PHONE_NUMBER = "Error: User with phone number %s already register";
-    public static final String  LESSON_PROGRAM_EXIST_MESSAGE = "Error: Course schedule cannot be selected for the same hour and day";
-    public static final String  MEET_EXIST_MESSAGE = "Error: Meet schedule cannot be selected for the same hour and day";
-    public static final String  MEET_NOT_FOUND_MESSAGE = "Error: Meet with meet id %d not found";
+    public static final String ALREADY_REGISTER_MESSAGE_PHONE_NUMBER = "Error: User with phone number %s already register";
+    public static final String LESSON_PROGRAM_EXIST_MESSAGE = "Error: Course schedule cannot be selected for the same hour and day";
+    public static final String MEET_EXIST_MESSAGE = "Error: Meet schedule cannot be selected for the same hour and day";
+    public static final String MEET_NOT_FOUND_MESSAGE = "Error: Meet with meet id %d not found";
 
-    public static final String  TIME_NOT_VALID_MESSAGE = "Error: start time must not be greater than or equal to stop time";
-    public static final String  STUDENT_INFO_NOT_FOUND = "Error: Student Info with id %d Not found";
-    public static final String  STUDENT_INFO_NOT_FOUND_BY_USERNAME = "Error: Student Info with Username %s Not found";
-    public static final String  ALREADY_REGISTER_MESSAGE_EMAIL= "Error: User with email address %s already register";
-    public static final String  ALREADY_REGISTER_LESSON_NAME = "Error: Student Info with lesson name %s already register";
+    public static final String COURSE_CREDIT_SCORE_IS_NOT_ENOUGH = "Error: The minimum course credit score must be 30 points.";
+    public static final String TIME_NOT_VALID_MESSAGE = "Error: start time must not be greater than or equal to stop time";
+    public static final String STUDENT_INFO_NOT_FOUND = "Error: Student Info with id %d Not found";
+    public static final String STUDENT_INFO_NOT_FOUND_BY_USERNAME = "Error: Student Info with Username %s Not found";
+    public static final String ALREADY_REGISTER_MESSAGE_EMAIL = "Error: User with email address %s already register";
+    public static final String ALREADY_REGISTER_LESSON_NAME = "Error: Student Info with lesson name %s already register";
 
-    public static final String  ALREADY_SEND_A_MESSAGE_TODAY = "Error: You already sent a message with this e-mail address today";
+    public static final String ALREADY_SEND_A_MESSAGE_TODAY = "Error: You already sent a message with this e-mail address today";
 
-
+    public static final String EDUCATION_TERM_NOT_FOUND_MESSAGE = "Error: Education Term with id %d not found";
+    public static final String EDUCATION_TERM_NOT_FOUND_MESSAGE_BY_TERM_AND_YEAR = "Error: Education Term with TERM AND YEAR already register";
+    public static final String EDUCATION_START_DATE_IS_SMALLER_THAN_END_DATE  = "Error: The start date cannot be less than the end date.";
+    public static final String EDUCATION_START_DATE_IS_SMALLER_THAN_LAST_REGISTRATION_DATE  = "Error: The start date cannot be less than the last registration date.";
+    public static final String EDUCATION_END_DATE_IS_SMALLER_THAN_LAST_REGISTRATION_DATE  = "Error: The end date cannot be less than the last registration date.";
 
 
 }
