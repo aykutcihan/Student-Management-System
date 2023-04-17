6371788ac0758f5e84651a5e992e25a8d18f4de9
Mehmet Akgul <mehmetakgul.dev@gmail.com>
2023-04-17 18:24:48 +0300

    unused imports are cleared in ContactMessageRequest.java

--- src/main/java/com/project/schoolmanagment/payload/request/ContactMessageRequest.java
+++ src/main/java/com/project/schoolmanagment/payload/request/ContactMessageRequest.java
@@ -1,7 +1,6 @@
 package com.project.schoolmanagment.payload.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
+ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +11,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
-import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
