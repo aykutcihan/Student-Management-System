565295520c3c65d9d2d2e51c3426bf3285f6a208
Mehmet Akgul <mehmetakgul.dev@gmail.com>
2023-05-09 22:18:18 +0300

    The order of the email animation has been changed.

--- src/main/java/com/project/schoolmanagment/payload/request/StudentRequest.java
+++ src/main/java/com/project/schoolmanagment/payload/request/StudentRequest.java
@@ -22,19 +22,18 @@ public class StudentRequest extends BaseUserRequest {
     @NotNull(message = "Please enter mother name")
     @Size(min = 2, max = 16, message = "Your mother name should be at least 2 characters")
     @Pattern(regexp="\\A(?!\\s*\\Z).+",message="Your mother name must consist of the characters a-z and 0-9.")
-
     private String motherName;
 
     @NotNull(message = "Please enter father name")
     @Size(min = 2, max = 16, message = "Your father name should be at least 2 characters")
     @Pattern(regexp="\\A(?!\\s*\\Z).+",message="Your father name must consist of the characters a-z and 0-9.")
-
     private String fatherName;
 
-    @Email(message = "Please enter valid email")
+
     @NotNull(message = "Please enter your email")
     @Size(min = 5, max = 20, message = "Your email should be at least 5 characters")
     @Column(nullable = false, unique = true, length = 20)
+    @Email(message = "Please enter valid email")
     private String email;
 
     @NotNull(message = "Please select advisor teacher")
